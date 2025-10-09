0736b52ae810c9c9431a7df93a0c58e28e3a47a7
Tamil Prabha <me.tamilprabha@gmail.com>
2025-10-09 13:04:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <div className="App">
+      <h1>Hello, Taskmate!</h1>
+    </div>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>This is the Header</h1>
+    </div>
+  )
+}
+
+export default Header
