3473708248b1da90cc63328c8535187efec6c173
Tamil Prabha <me.tamilprabha@gmail.com>
2025-10-25 22:13:09 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-
+import { useState } from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
 function App() {
+
+  let [count, setCount] = useState(0);
+
+  function handleAdd(){
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+
+  }
+
+  function handleSub(){
+    setCount(count - 1);
+  }
+
+  function handleReset(){
+    setCount(0);
+  }
+
   return (
-    <>
-    <Header />
+
     <div className="App">
-      <h1>Hello, Taskmate!</h1>
+      <div className="box">
+        <p>{count}</p>
+        <button className='add' onClick={handleAdd}>ADD</button>
+        <button className='sub' onClick={handleSub}>SUB</button>
+        <button className='reset' onClick={handleReset}>RESET</button>
+        </div>
     </div>
-    <Footer />
-    </>
+
   );
 }
 
